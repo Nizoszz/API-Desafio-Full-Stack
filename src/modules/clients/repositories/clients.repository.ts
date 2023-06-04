015a0e42ca248d61055491bda9e913f17695c0cd
Andrew <andrewairamdasilva@gmail.com>
2023-06-04 13:56:17 -0300

    added modules app and auth

---
+++ src/modules/clients/repositories/clients.repository.ts
@@ -0,0 +1,15 @@
+import { CreateClientDto } from '../dto/create-client.dto';
+import { UpdateClientDto } from '../dto/update-client.dto';
+import { Client } from '../entities/client.entity';
+
+export abstract class ClientsRepository {
+  abstract create(
+    data: CreateClientDto,
+    userId: string,
+  ): Promise<Client> | Client;
+  abstract findAll(userId: string): Promise<Client[]> | Client[];
+  abstract findOne(id: string): Promise<Client> | Client;
+  abstract findByEmail(email: string): Promise<Client> | Client;
+  abstract update(id: string, data: UpdateClientDto): Promise<Client> | Client;
+  abstract delete(id: string): Promise<void> | void;
+}
